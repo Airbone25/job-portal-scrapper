0c4255ae0afcf575ea0eb21b73eaed032a5c3d79
Keshav Mehra <keshavmehra2005@gmail.com>
2025-02-27 11:34:35 +0530

    Added Database(MongoDB)

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const express = require('express');
+const mongoose = require('mongoose');
+const Job = require('./models/Job');
 const app = express();
 
-const scrapData = async () => {
+mongoose.connect('mongodb+srv://keshavmehra2005:LeoMessi10@cluster0.zky0x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+const db = mongoose.connection;
+db.on('error', ()=>console.error('Error connecting to database'));
+db.once('open',()=>{
+    console.log('Connected to database');
+})
+
+const scrapData = async (url,parentSelector,titleSelector,companySelector) => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto('https://www.foundit.in/srp/results?query=%22%22&quickApplyJobs=true&searchId=f97a63ce-5eb5-4085-9e2a-35c837e728fa');
+    await page.goto(url);
     const content = await page.content();
     const $ = cheerio.load(content);
     const jobs = [];
-    $('.infoSection').each((index, element) => {
-        const jobTitle = $(element).find('.jobTitle').text().trim();
-        const companyName = $(element).find('.companyName > p').text().trim();
+    $(parentSelector).each((index, element) => {
+        const jobTitle = $(element).find(titleSelector).text().trim();
+        const companyName = $(element).find(companySelector).text().trim();
         jobs.push({jobTitle,companyName});
     });
     await browser.close();
     return jobs;
 }
 
+const options = [
+    {
+        url: 'https://www.foundit.in/srp/results?query=%22%22&quickApplyJobs=true&searchId=f97a63ce-5eb5-4085-9e2a-35c837e728fa',
+        parentSelector: '.infoSection',
+        titleSelector: '.jobTitle',
+        companySelector: '.companyName > p'
+    }
+]
+
 app.get('/scrape', async (req, res) => {
-    const scrapedJobs = await scrapData();
+    const scrapedJobs = [];
+    for(let i=0;i<options.length;i++){
+        const {url,parentSelector,titleSelector,companySelector} = options[i];
+        const jobs = await scrapData(url,parentSelector,titleSelector,companySelector);
+        scrapedJobs.push(...jobs);
+    }
+    for(let i=0;i<scrapedJobs.length;i++){
+        const job = new Job(scrapedJobs[i]);
+        await job.save();
+    }
     res.send(scrapedJobs);
 });
 
